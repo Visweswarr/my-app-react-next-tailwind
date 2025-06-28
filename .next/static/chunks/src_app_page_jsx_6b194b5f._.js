46b09fb3c73ffb0fabb22c8311413373e7ae0649
Visweswarr <77443682+Visweswarr@users.noreply.github.com>
2025-06-28 23:04:13 +0300

    Add .next/static/chunks/src_app_page_jsx_6b194b5f._.js 1751141050629

---
+++ .next/static/chunks/src_app_page_jsx_6b194b5f._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_framer-motion_dist_es_24e93cb0._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_fa3ea29e._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_three_build_three_core_df0bde7e.js",
+  "static/chunks/node_modules_three_build_three_module_230a9f89.js",
+  "static/chunks/node_modules_three_build_three_module_90b4155f.js",
+  "static/chunks/node_modules_react-reconciler_25c8ec02._.js",
+  "static/chunks/node_modules_@react-three_fiber_dist_dc1c2c82._.js",
+  "static/chunks/node_modules_49f132e7._.js",
+  "static/chunks/src_5fc3c957._.js"
+],
+    source: "dynamic"
+});
